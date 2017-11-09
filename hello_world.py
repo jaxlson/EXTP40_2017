8e0409056ee5e639433f59b5fe2fd23ef7163819
JosefineA <lan13jax@student.lu.se>
2017-11-09 10:53:26 +0100

    Test of TKinter toolkit

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,12 @@
-print "hello world"
+
+import Tkinter
+from Tkinter import tkinter
+
+def main():
+    print "Start TKinter window"
+    # Open a small window
+    top = Tkinter.Tk()
+    top.mainloop()
+    
+if __name__ == '__main__':
+    main()
