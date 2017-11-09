b54b01d6313f01af33b1504f1dd7854760abf161
JosefineA <lan13jax@student.lu.se>
2017-11-09 11:00:28 +0100

    Implement button

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,20 @@
 
 import Tkinter
 from Tkinter import tkinter
+import tkMessageBox
+from Tkconstants import COMMAND
 
 def main():
     print "Start TKinter window"
     # Open a small window
     top = Tkinter.Tk()
+    
+    def helloCallBack():
+        tkMessageBox.showinfo("Hello Python", "Hello World")
+        
+    B = Tkinter.Button(top, text = "Hello", command = helloCallBack)
+    
+    B.pack()    
     top.mainloop()
     
 if __name__ == '__main__':
