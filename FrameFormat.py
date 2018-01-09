7bd7be807cbcd609effc63097027853b76db34ae
JosefineA <lan13jax@student.lu.se>
2018-01-09 17:18:06 +0100

    FrameFormat changes

--- FrameFormat.py
+++ FrameFormat.py
@@ -5,12 +5,13 @@ Created on 28 nov. 2017
 '''
 from Tkinter import Label, Entry, Button, IntVar, StringVar
 import ttk
+from Tkconstants import SUNKEN
 
 class FrameFormat(object):
 
     def __init__(self,gui, master, frame):
         self.main_label = Label(frame, text = "TIMESAT image viewer")
-        self.address_label = Label(frame, text = "D:/-LTH-/-HT 17-/GIT-projekt i Python/Projekt/EXTP40_2017/EXTP40_2017/wa_cl00011.img")
+        self.address_label = Label(frame, text = "D:/-LTH-/-HT 17-/GIT-projekt i Python/Projekt/EXTP40_2017/EXTP40_2017/wa_cl00011.img", relief=SUNKEN)
         self.type_label = Label(frame, text = "Image file type")
         type_box_value = StringVar()
         self.type_box = ttk.Combobox(frame, textvariable=type_box_value, state='readonly')
@@ -34,18 +35,18 @@ class FrameFormat(object):
         self.draw_button = Button(frame, text="Draw", command=lambda: gui.display(self.type_box.get(), self.order_box.get(), self.row_entry.get(),self.col_entry.get()))
         
         self.main_label.grid(row=0, column=0, sticky='w')
-        self.address_label.grid(row=0, column=1)
-        self.type_label.grid(row=1, column=0, sticky='w')
-        self.type_box.grid(row=1,column=1,sticky='e')
-        self.order_label.grid(row=2, column=0, sticky='w')
-        self.order_box.grid(row=2,column=1,sticky='e')
+        self.address_label.grid(row=1, column=0)
+        self.type_label.grid(row=2, column=0, sticky='w')
+        self.type_box.grid(row=2,column=1,sticky='e')
+        self.order_label.grid(row=3, column=0, sticky='w')
+        self.order_box.grid(row=3,column=1,sticky='e')
 
-        self.row_label.grid(row=3, column=0, sticky='w')
-        self.row_entry.grid(row=3, column=1, sticky='we')
-        self.col_label.grid(row=4, column=0, sticky='w')
-        self.col_entry.grid(row=4, column=1, sticky='we')
+        self.row_label.grid(row=4, column=0, sticky='w')
+        self.row_entry.grid(row=4, column=1, sticky='we')
+        self.col_label.grid(row=5, column=0, sticky='w')
+        self.col_entry.grid(row=5, column=1, sticky='we')
 
-        self.draw_button.grid(row=4, column=2, sticky='e')
+        self.draw_button.grid(row=5, column=2, sticky='e')
         
     #Only numbers in the entries
     def nbr_check(self, new_text):
